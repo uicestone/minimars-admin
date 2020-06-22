aab7ae9c6eb768a0eae52c365655b33871673862
Uice Lu <uicestone@gmail.com>
2020-06-22 23:22:39 +0800

    fix: admin can add coupon card

--- src/components/Membership.vue
+++ src/components/Membership.vue
@@ -5,7 +5,7 @@
       md-menu.pull-right(v-if="cardItems")
         md-button.md-info.md-sm(md-menu-trigger) 购卡
         md-menu-content
-          md-menu-item(v-for='cardType in $cardTypes', v-if="cardType.type !== 'coupon'", :key='cardType.id', @click='createCard(cardType)') {{ cardType.title }}
+          md-menu-item(v-for='cardType in $cardTypes', v-if="$user.role==='admin'||cardType.type !== 'coupon'", :key='cardType.id', @click='createCard(cardType)') {{ cardType.title }}
           md-menu-item(@click="receiveGiftCard") 接收礼品卡
   payments-card(:payments="paymentItems" :pay="pay")
 </template>
