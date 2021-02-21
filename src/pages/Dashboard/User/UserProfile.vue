0095f1ddbd53a2ffbe8aadb5123ba513a97b4123
Uice Lu <uicestone@gmail.com>
2021-02-21 21:42:43 +0800

    disable user save if mobile is not 11 digit

--- src/pages/Dashboard/User/UserProfile.vue
+++ src/pages/Dashboard/User/UserProfile.vue
@@ -96,7 +96,7 @@
                   label 重置密码
                   md-input(v-model='user.password', type='password', autocomplete='new-password')
             .md-layout-item.md-size-100.text-right
-              md-button.md-success.mt-4(type='submit' :class="{'md-raised':!user.id,'md-simple':user.id}") 保存
+              md-button.md-success.mt-4(type='submit' :class="{'md-raised':!user.id,'md-simple':user.id}" :disabled="!userValidated") {{ user.id ? "更新" : "创建" }}
               md-button.md-primary.mt-4.pull-right(@click="createBooking()" v-if="user.id && user.role === 'customer'") 门票预约
               md-button.md-success.mt-4.mr-1.pull-right(@click="createBooking('food')" v-if="user.id && user.role === 'customer'") 吧台消费
       bookings-card(title="近期门票预约" type="play" :bookings="userBookings.filter(b=>b.type==='play')" :customer="user" v-if="user.role === 'customer'")
@@ -143,6 +143,11 @@ export default class UserProfile extends Vue {
 
   get creatable() {
     return !this.user.id && this.user.mobile?.length === 11;
+  }
+
+  get userValidated() {
+    if (!this.user.mobile || this.user.mobile.length !== 11) return false;
+    return true;
   }
 
   async save() {
